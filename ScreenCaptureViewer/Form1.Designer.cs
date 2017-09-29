621e9d888aef3187f84dcfd3eb63e07abab8fc07
Sam Salefske <Sam.salefske@gmail.com>
2017-09-28 21:05:30 -0400

    Adding Existing Application
    Adding Existing Application

---
+++ ScreenCaptureViewer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ScreenCaptureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTimeValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(212, 20);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(230, 10);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Open Folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(31, 152);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(235, 271);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 504);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(293, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(329, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(857, 473);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTimeValue
+            // 
+            this.lblTimeValue.AutoSize = true;
+            this.lblTimeValue.Location = new System.Drawing.Point(116, 544);
+            this.lblTimeValue.Name = "lblTimeValue";
+            this.lblTimeValue.Size = new System.Drawing.Size(0, 13);
+            this.lblTimeValue.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 608);
+            this.Controls.Add(this.lblTimeValue);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTimeValue;
+    }
+}
+
---
+++ ScreenCaptureViewer/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScreenCaptureViewer
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd.Description = "Select Path For All Employees";
+            DialogResult result = fbd.ShowDialog();
+            if(result==DialogResult.OK || result==DialogResult.Yes)
+            {
+                txtPath.Text = fbd.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("Must Select A Valid Path To Continue");
+            }
+            fbd.Dispose();
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            treeView1.Visible = true;
+            trackBar2.Visible = true;
+            
+            trackBar2.SetRange(08, 18);
+            lblTimeValue.Text = "8:00 AM";
+            
+            //Add Directories
+            loadDirectories("07_30_00", "08_30_59");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.Visible = false;
+            trackBar2.Visible = false;
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text.Contains(".png"))
+            {
+                if (e.Node.Checked)
+                {
+                    UncheckAllNodes(e.Node.TreeView.Nodes, e.Node);
+                    pictureBox1.Image = Image.FromFile(fbd.SelectedPath + "\\" + e.Node.Parent.Text + "\\" + e.Node.Text);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                else
+                {
+                    pictureBox1.Image = null;
+                }
+            }
+            else
+            {
+                //Checking if it's already checked allows us to break out of the endless loop this creates by setting the node to unchecked (casues the treeview1_aftercheck to kick off again)
+                if (e.Node.Checked == true)
+                {
+                    e.Node.Checked = false;
+                }
+                else
+                {
+                    //dont do anything because it's already unchecked.
+                }
+            }
+        }
+
+        private void loadDirectories()
+        {
+            treeView1.Nodes.Clear();
+            var stack = new Stack<TreeNode>();
+            var rootDirectory = new DirectoryInfo(txtPath.Text);
+            var node = new TreeNode(rootDirectory.Name) { Tag = rootDirectory };
+            stack.Push(node);
+
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+                var directoryInfo = (DirectoryInfo)currentNode.Tag;
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    var childDirectoryNode = new TreeNode(directory.Name) { Tag = directory };
+                    currentNode.Nodes.Add(childDirectoryNode);
+                    stack.Push(childDirectoryNode);
+                }
+                foreach (var file in directoryInfo.GetFiles())
+                    currentNode.Nodes.Add(new TreeNode(file.Name));
+            }
+            treeView1.Nodes.Add(node);
+        }
+
+        private void loadDirectories(string filterStart, string filterEnd)
+        {
+            treeView1.Nodes.Clear();
+            var stack = new Stack<TreeNode>();
+            var rootDirectory = new DirectoryInfo(txtPath.Text);
+            var node = new TreeNode(rootDirectory.Name) { Tag = rootDirectory };
+            stack.Push(node);
+
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+                var directoryInfo = (DirectoryInfo)currentNode.Tag;
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    var childDirectoryNode = new TreeNode(directory.Name) { Tag = directory };
+                    currentNode.Nodes.Add(childDirectoryNode);
+                    stack.Push(childDirectoryNode);
+                }
+                foreach (var file in directoryInfo.GetFiles())
+                //if filename is within +- 30 minutes of filter than add it
+                {
+                    var tempFilterStart = filterStart.Replace('_', ':');
+                    var tempFilterEnd = filterEnd.Replace('_', ':');
+                    var tempFile = file.ToString().Replace('_', ':').Remove(file.ToString().IndexOf('.'),4);
+
+                    if((Convert.ToDateTime(tempFile)>Convert.ToDateTime(tempFilterStart))&&(Convert.ToDateTime(tempFile)<Convert.ToDateTime(tempFilterEnd)))
+                    {
+                        currentNode.Nodes.Add(new TreeNode(file.Name));
+                    }
+                }
+            }
+            treeView1.Nodes.Add(node);
+            treeView1.ExpandAll();
+        }
+
+        public void UncheckAllNodes(TreeNodeCollection nodes, TreeNode currentTreeNode)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                if(node != currentTreeNode)
+                {
+                    node.Checked = false;
+                    CheckChildren(node, false, currentTreeNode);
+                }
+            }
+        }
+
+        private void CheckChildren(TreeNode rootNode, bool isChecked, TreeNode currentTreeNode)
+        {
+            foreach (TreeNode node in rootNode.Nodes)
+            {
+                if (node != currentTreeNode)
+                {
+                    CheckChildren(node, isChecked, currentTreeNode);
+                    node.Checked = isChecked;
+                }
+            }
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            //don't allow folders to be selected....
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if(trackBar2.Value.ToString() == "8")
+            {
+                lblTimeValue.Text = "8:00 AM";
+                loadDirectories("07_30_00", "08_30_59");
+            }
+            if (trackBar2.Value.ToString() == "9")
+            {
+                lblTimeValue.Text = "9:00 AM";
+                loadDirectories("08_30_00", "09_30_59");
+            }
+            if (trackBar2.Value.ToString() == "10")
+            {
+                lblTimeValue.Text = "10:00 AM";
+                loadDirectories("09_30_00", "10_30_59");
+            }
+            if (trackBar2.Value.ToString() == "11")
+            {
+                lblTimeValue.Text = "11:00 AM";
+                loadDirectories("10_30_00", "11_30_59");
+            }
+            if (trackBar2.Value.ToString() == "12")
+            {
+                lblTimeValue.Text = "12:00 PM";
+                loadDirectories("11_30_00", "12_30_59");
+            }
+            if (trackBar2.Value.ToString() == "13")
+            {
+                lblTimeValue.Text = "1:00 PM";
+                loadDirectories("12_30_00", "13_30_59");
+            }
+            if (trackBar2.Value.ToString() == "14")
+            {
+                lblTimeValue.Text = "2:00 PM";
+                loadDirectories("13_30_00", "14_30_59");
+            }
+            if (trackBar2.Value.ToString() == "15")
+            {
+                lblTimeValue.Text = "3:00 PM";
+                loadDirectories("14_30_00", "15_30_59");
+            }
+            if (trackBar2.Value.ToString() == "16")
+            {
+                lblTimeValue.Text = "4:00 PM";
+                loadDirectories("15_30_00", "16_30_59");
+            }
+            if (trackBar2.Value.ToString() == "17")
+            {
+                lblTimeValue.Text = "5:00 PM";
+                loadDirectories("16_30_00", "17_30_59");
+            }
+            if (trackBar2.Value.ToString() == "18")
+            {
+                lblTimeValue.Text = "6:00 PM";
+                loadDirectories("17_30_00", "18_30_59");
+            }
+        }
+    }
+}
